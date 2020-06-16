17d375c7a271a0c6a13523ec838c9f00546c1d1d
Thiago Guimarães <penks@live.com>
2020-06-15 23:30:33 -0300

    add dockerfile and nginx.config

---
+++ front-end/src/app/views/input-transaction-future/input-transaction-future.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-future',
+  templateUrl: './input-transaction-future.component.html',
+  styleUrls: ['./input-transaction-future.component.css']
+})
+export class InputTransactionFutureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
